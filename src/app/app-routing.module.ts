5c567fc29bb0fa65aaa62664e943e7e2738f169e
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-13 20:34:18 +0200

    imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersViewComponent } from './characters-view/characters-view/characters-view.component';
+import { HomeCompComponent } from './home-comp/home-comp.component';
+import { AboutComponent } from './about/about.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeCompComponent},
+  {path: 'characters', component: CharactersViewComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'lista-characters', component: CharacterListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { CharactersViewModule } from './characters-view/characters-view.module';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeCompComponent } from './home-comp/home-comp.component';
-import { CharactersViewComponent } from './characters-view/characters-view/characters-view.component';
 import { AboutComponent } from './about/about.component';
-import { AddCharacterComponent } from './conponents/add-character/add-character.component';
 import { CharacterDetailsComponent } from './components/character-details/character-details.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -28,11 +26,7 @@ import { CharacterListComponent } from './components/character-list/character-li
     BrowserModule,
     AppRoutingModule,
     CharactersViewModule,
-    RouterModule.forRoot([
-      {path: '', component: HomeCompComponent},
-      {path: 'characters', component: CharactersViewComponent},
-      {path: 'about', component: AboutComponent},
-    ]),
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
